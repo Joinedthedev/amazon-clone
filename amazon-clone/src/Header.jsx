7d2a957b07d099d9243b02285148d8ddb748928f
Salim <salimbabaji@gmail.com>
2023-09-18 20:47:51 +0100

    -finished header

--- amazon-clone/src/Header.jsx
+++ amazon-clone/src/Header.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 const Header = () => {
   return (
     <div className="header">
-      Header
-      <img className="header-logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="Amazon" />
+      <img
+        className="header-logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        alt="Amazon"
+      />
       <div className="header-search">
         <input className="header-searchInput" type="text" />
+        <SearchIcon className="header-searchIcon" />
       </div>
       <div className="header-nav">
         <div className="header-option">
@@ -23,6 +29,13 @@ const Header = () => {
         <div className="header-option">
           <span className="header-optionLineOne">Your</span>
           <span className="header-optionLineTwo"> prime</span>
+        </div>
+
+        <div className="header-optionBasket">
+          <ShoppingCartIcon/> 
+        
+          <span className="header-optionLineTwo header-BasketCount">0</span>
+
         </div>
       </div>
     </div>
