9db2f35514fcba4422f9889d23a9056c4a48ef4b
Salim <salimbabaji@gmail.com>
2023-09-25 16:47:37 +0100

    removed random dynamic render

--- amazon-clone/src/components/Home.jsx
+++ amazon-clone/src/components/Home.jsx
@@ -5,52 +5,33 @@ import Product from "./Product";
 const Home = () => {
   const URL = "https://fakestoreapi.com/products";
   const [products, setProducts] = useState([]);
-  const productsPerRow = [2, 4, 3]; // Determines the number of products per row.
-
-  //   the essential use of this entire fetching process is to dynamically render products from an api call
-  //   The useEffect hook is needed in order to fetch the data
+  const productsPerRow = [4, 4, 4]; // Adjust this as needed
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        //this will check if the http response is okay. If it is everything will proceed and if its not an error will be
-        //thrown
         const response = await fetch(URL);
         if (!response.ok) {
-          throw new Error("NETWORK RESPONSE FAILED");
+          throw new Error("Network response was not ok");
         }
         const data = await response.json();
-        setProducts(data); //this sets the data into our state array "prodcuts"
-
-        // this will Shuffle the products randomly
-        const shuffledProducts = [...data]; //Creates a new array and stores the products in them
-        for (let i = shuffledProducts.length - 1; i > 0; i--) { //loops through the array in reverse order
-          const j = Math.floor(Math.random() * (i + 1)); //creates random index j between 0 and i
-          [shuffledProducts[i], shuffledProducts[j]] = [
-            shuffledProducts[j],
-            shuffledProducts[i],
-          ];
-        }
-
-        // Distribute products into rows based on the productsPerRow array
-        const productRows = [];
-        let currentIndex = 0;
-        for (const productsInRow of productsPerRow) {
-          const row = shuffledProducts.slice(
-            currentIndex,
-            currentIndex + productsInRow
-          );
-          productRows.push(row);
-          currentIndex += productsInRow;
-        }
-
-        setProducts(productRows);
+        console.log(data);
+        setProducts(data);
       } catch (error) {
-        console.error("Error fetching data", error);
+        console.error("Error fetching data:", error);
       }
     };
     fetchData();
-  }, []); //this bit ensures that its only ran once
+  }, []);
+
+  // Distribute products into rows based on the productsPerRow array
+  const productRows = [];
+  let currentIndex = 0;
+  for (const productsInRow of productsPerRow) {
+    const row = products.slice(currentIndex, currentIndex + productsInRow);
+    productRows.push(row);
+    currentIndex += productsInRow;
+  }
 
   return (
     <div className="home">
@@ -62,7 +43,7 @@ const Home = () => {
         />
 
         {Array.isArray(products) ? (
-          products.map((row, rowIndex) => (
+          productRows.map((row, rowIndex) => (
             <div className="home-row" key={rowIndex}>
               {row.map((product) => (
                 <Product
